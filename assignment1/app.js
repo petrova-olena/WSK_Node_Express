552138252e33ed18aa5f770f5468289e0a0a7f2e
petrova-olena <petrova.olena.anatoliivna@gmail.com>
2026-04-01 13:10:52 +0300

    Add assignment1, done

--- assignment1/app.js
+++ assignment1/app.js
@@ -1,4 +1,3 @@
-
 import express from 'express';
 const app = express();
 const port = 3000;
@@ -6,11 +5,11 @@ const port = 3000;
 app.get('/api/v1/cats', (req, res) => {
   res.json({
     cat_id: 1,
-    name: "Murka",
-    birthdate: "2020-05-10",
+    name: 'Murka',
+    birthdate: '2020-05-10',
     weight: 4.2,
-    owner: "Elena",
-    image: "https://loremflickr.com/320/240/cat"
+    owner: 'Olena',
+    image: 'http://localhost:3000/public/test.jpg',
   });
 });
 
@@ -19,4 +18,3 @@ app.use('/public', express.static('public'));
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
-
